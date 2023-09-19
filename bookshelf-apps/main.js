f3f5140cd9b9f40daf0e539ab9718cb84afbe8f3
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-09-19 15:01:31 +0700

    adding optional seach and notif

--- bookshelf-apps/main.js
+++ bookshelf-apps/main.js
@@ -1,29 +1,69 @@
+const STORAGE_KEY = 'books';
+const RENDER_EVENT = 'render books'
+
 const books = []
-let RENDER_EVENT = 'render books'
-
-document.addEventListener(RENDER_EVENT, function () {
-  const unread = document.getElementById('incompleteBookshelfList');
-  unread.innerHTML = '';
  
-  const readed = document.getElementById('completeBookshelfList');
-  readed.innerHTML = '';
-
-  for (const book of books) {
-    const bookShelf = makeBookshelf(book);
-    if(!book.isCompleted) {
-      unread.append(bookShelf)
-    } else {
-      readed.append(bookShelf)
-    }
-  }
-});
-
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('inputBook')
   form.addEventListener('submit', (e) => {
     e.preventDefault()
     addBook()    
   })
+  loadDataFromStorage();
+})
+
+const saveData = () => {
+    const parsed = JSON.stringify(books);
+    localStorage.setItem(STORAGE_KEY, parsed);
+}
+
+const loadDataFromStorage = () => {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+ 
+  if (data !== null) {
+    for (const book of data) {
+      books.push(book);
+    }
+  }
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+const toggleInput = document.querySelectorAll('.hidden-form');
+toggleInput.forEach(toggleBook => {
+  toggleBook.addEventListener('click', () => {
+    const toggleHidden = document.getElementById('toggle-hidden');
+    if (toggleHidden.hidden) {
+      toggleHidden.removeAttribute('hidden');
+    } else {
+      toggleHidden.setAttribute('hidden', true);
+    }
+  });
+});
+
+document.addEventListener(RENDER_EVENT, function () {
+  const searchTerm = document.getElementById('searchBookTitle').value.toLowerCase();
+  const unread = document.getElementById('incompleteBookshelfList');
+  const readed = document.getElementById('completeBookshelfList');
+  unread.innerHTML = '';
+  readed.innerHTML = '';
+
+  for (const book of books) {
+    const bookTitle = book.title.toLowerCase();
+    if (bookTitle.includes(searchTerm)) {
+      const bookShelf = makeBookshelf(book);
+      if (!book.isCompleted) {
+        unread.append(bookShelf);
+      } else {
+        readed.append(bookShelf);
+      }
+    }
+  }
+});
+
+const searchBook = document.getElementById('searchBookTitle')
+  searchBook.addEventListener('input', () => {
+  document.dispatchEvent(new Event(RENDER_EVENT));
 })
 
 const addBook = () => {
@@ -41,10 +81,15 @@ const addBook = () => {
   document.getElementById('inputBookAuthor').value = '';
   document.getElementById('inputBookYear').value = '';
   document.getElementById('inputBookIsComplete').checked = false;
+
+  document.getElementById('toggle-hidden').setAttribute('hidden', true);
+
+  notification('behasil menambahkan buku')
+  saveData()
 }
 
 const helper = (id, title, author, year, isCompleted) => {
-  const value = { id, title, author, books, year, isCompleted }
+  const value = { id, title, author, year, isCompleted }
   return value
 }
 
@@ -59,7 +104,7 @@ const makeBookshelf = (book) => {
   year.innerText = `Tahun: ${book.year}`
 
   const buttonRemove = document.createElement('button')
-  buttonRemove.classList.add('red')
+  buttonRemove.classList.add('button-book','danger-button')
   buttonRemove.innerText = 'Hapus buku'
 
   buttonRemove.addEventListener('click', () => {
@@ -71,7 +116,7 @@ const makeBookshelf = (book) => {
 
   if(book.isCompleted) {
     const buttonUndo = document.createElement('button')
-    buttonUndo.classList.add('green')
+    buttonUndo.classList.add('button-book','primary-button')
     buttonUndo.innerText = 'Belum Selesai dibaca'
 
     buttonUndo.addEventListener('click', () => {
@@ -81,7 +126,7 @@ const makeBookshelf = (book) => {
     divAction.append(buttonUndo, buttonRemove)
   } else {
     const buttonDone = document.createElement('button')
-    buttonDone.classList.add('green')
+    buttonDone.classList.add('button-book','primary-button')
     buttonDone.innerText = 'selesai dibaca'
 
     buttonDone.addEventListener('click', () => {
@@ -92,7 +137,7 @@ const makeBookshelf = (book) => {
   }
 
   const article = document.createElement('article')
-  article.classList.add('book_item')
+  article.classList.add('book-item')
   article.append(title, author, year, divAction)
 
   return article
@@ -103,6 +148,7 @@ const undoCompletedBook = (id) => {
 
   books[selectBook].isCompleted = false
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData()
 }
 
 const CompletedBook = (id) => {
@@ -112,6 +158,7 @@ const CompletedBook = (id) => {
 
   books[selectBook].isCompleted = true
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData()
 }
 
 const removeBook = (id) => {
@@ -121,6 +168,8 @@ const removeBook = (id) => {
  
   books.splice(target, 1);
   document.dispatchEvent(new Event(RENDER_EVENT));
+  notification('berhasil menghapus buku')
+  saveData()
 }
 
 const findId = (id) => {
@@ -132,3 +181,17 @@ const findId = (id) => {
  
   return -1;
 }
+
+const notification = (text) => {
+  const notif = document.getElementsByClassName('notification')[0]
+  const textNotif = document.getElementById('text-notif')
+
+  notif.removeAttribute('hidden')
+  textNotif.innerText = text
+
+  setTimeout(() => {
+    notif.setAttribute('hidden', true)
+    textNotif.innerText = ''
+  },3000)
+}
+
