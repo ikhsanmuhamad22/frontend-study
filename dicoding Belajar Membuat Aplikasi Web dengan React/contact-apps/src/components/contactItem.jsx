877ed66d9da3a4150ccf40c20e077b98fc678300
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-10-02 09:44:20 +0700

    adding DeleteButton as implementasi state react

--- dicoding Belajar Membuat Aplikasi Web dengan React/contact-apps/src/components/contactItem.jsx
+++ dicoding Belajar Membuat Aplikasi Web dengan React/contact-apps/src/components/contactItem.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ContactItemBody from './ContactItemBody';
-import ContactItemImage from './contactItemImage';
+import ContactItemImage from './ContactItemImage';
+import DeleteButton from './DeleteButton';
  
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({ imageUrl, name, tag, id, onDelete }) {
  return (
    <div className="contact-item">
      <ContactItemImage imageUrl={imageUrl} />
      <ContactItemBody name={name} tag={tag} />
+     <DeleteButton id={id} onDelete={onDelete} />
    </div>
  );
 }
- 
-export default ContactItem;
+
+export default ContactItem
