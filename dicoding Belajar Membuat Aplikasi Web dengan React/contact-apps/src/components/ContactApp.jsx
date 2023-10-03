c609bdabcb351a10e38e5f043087ecf1342268ff
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-10-03 17:24:47 +0700

    fixing contact-apps

--- dicoding Belajar Membuat Aplikasi Web dengan React/contact-apps/src/components/ContactApp.jsx
+++ dicoding Belajar Membuat Aplikasi Web dengan React/contact-apps/src/components/ContactApp.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactList from './ContactList';
 import { getData } from '../utils/data';
+import ContactInput from './ContactInput';
  
 class ContactApp extends React.Component {
  constructor(props) {
@@ -10,16 +11,36 @@ class ContactApp extends React.Component {
    }
  
    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+   this.onAddContactHandler = this.onAddContactHandler.bind(this);
  }
  
  onDeleteHandler(id) {
    const contacts = this.state.contacts.filter(contact => contact.id !== id);
    this.setState({ contacts });
  }
+
+ onAddContactHandler({ name, tag }) {
+  this.setState((prevState) => {
+    return {
+      contacts: [
+        ...prevState.contacts,
+        {
+          id: +new Date(),
+          name,
+          tag,
+          imageUrl: '/images/default.jpg',
+        }
+      ]
+    }
+  });
+}
  
  render() {
    return (
      <div className="contact-app">
+      <h1>Aplikasi kontak</h1>
+      <h2>Tambah Kontak</h2>
+      <ContactInput addContact={this.onAddContactHandler} />
        <h1>Daftar Kontak</h1>
        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
      </div>
