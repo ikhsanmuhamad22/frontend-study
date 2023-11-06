d502502333f902ad603b4c00f881dc252f41ca5f
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-11-06 21:47:30 +0700

    tipe data array

--- typescript/typescript_dasar/test/typeData.test.ts
+++ typescript/typescript_dasar/test/typeData.test.ts
@@ -1,3 +1,4 @@
+// data types primitif
 describe('Data type', function() {
   it('should must declare', function() {
     const name: string = 'ikhsan maulana';
@@ -7,5 +8,31 @@ describe('Data type', function() {
     console.info(name)
     console.info(balance)
     console.info(isVip)
+  })
+})
+
+// data types array
+describe('Data type array', function() {
+  it('should must declare an array', function() {
+    const name: string[] = ['ikhsan', 'muhamad', 'maulana']
+    const age: number[] = [19, 2003]
+
+    console.info(name)
+    console.info(age)
+  })
+
+  it('should support an ReadOnlyArray', function() {
+    // adalah type data array yang tidak dapat diubah
+    const hobbies: ReadonlyArray<string> = ["membaca", "menulis"]
+
+    console.info(hobbies)
+  })
+
+  it('should support an tuple array', function() {
+    // adalah type data array yang sudah ditentukan diawal dan tidak bisa diubah
+    const person: readonly [string, number, string]= ['ikhsan', 19, "cianjur"]
+
+    console.info(person[0])
+    console.info(person[2])
   })
 })
