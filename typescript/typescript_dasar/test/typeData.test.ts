17f06e5676c67fa575ca3bf09884fe055c19ffcc
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-11-06 21:25:00 +0700

    type data

---
+++ typescript/typescript_dasar/test/typeData.test.ts
@@ -0,0 +1,11 @@
+describe('Data type', function() {
+  it('should must declare', function() {
+    const name: string = 'ikhsan maulana';
+    const balance: number = 100
+    const isVip: boolean = true
+
+    console.info(name)
+    console.info(balance)
+    console.info(isVip)
+  })
+})
