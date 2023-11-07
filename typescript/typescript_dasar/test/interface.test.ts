dfc128fbd147747455eacf8c79411e2577073a23
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-11-07 12:53:15 +0700

    function interface

--- typescript/typescript_dasar/test/interface.test.ts
+++ typescript/typescript_dasar/test/interface.test.ts
@@ -9,4 +9,16 @@ describe('Interface', () => {
       nib: '23243123'
     }
   });
+
+  it('should support function interface', () => {
+    interface Addfunction {
+      (value1: number, value2: number): number
+    }
+
+    const add: Addfunction = (value1: number, value2: number) => {
+      return value1 + value2
+    }
+
+    expect(add(2,2)).toBe(4)
+  });
 });
