459c5cc2f4987f8a74b4ac99d45afee68ac722df
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-11-14 11:24:10 +0700

    add exercise array-in-object

--- typescript/exercise/array-in-object.ts
+++ typescript/exercise/array-in-object.ts
@@ -3,6 +3,21 @@
 // Inputan akan berupa array in object dengan struktur [ { name: namaSiswa, score: nilaiRapor, class: namaKelas } ].
 // Bagilah data input berdasarkan kelasnya.
 // Struktur output yang diharapkan yaitu => { namaKelas : [ { name: namaSiswa, score: nilaiRapor } ], … }.
+const arrInObj = (arr: any) => {
+    let newObj: any = {}
+    arr.map((val: any, index: number) => {
+       newObj[val.class] ? (newObj[val.class] = [
+        ...newObj[val.class],
+        { name: val.name, score: val.score }])
+        : (newObj[val.class] = [{ name: val.name, score: val.score }])
+    })
+    return newObj
+}
+console.log(arrInObj([
+    { name: 'Ujang', score: 70, class: 'melati'},
+    { name: 'Asep', score: 80, class: 'mawar'},
+    { name: 'Udin', score: 70, class: 'melati'}
+]))
 
 
 
@@ -12,6 +27,18 @@
 // Inputan akan berupa array in object dengan struktur [ { name: namaSiswa, score: nilaiRapor, class: namaKelas } ].
 // Bagilah data input berdasarkan score, jika score diatas 75 maka kategorinya “lulus” dan selain itu kategorinya “gagal”.
 // Struktur output yang diharapkan yaitu => { lulus : [ { name: namaSiswa, score: nilaiRapor, class: namaKelas } ], gagal : [ { name: namaSiswa, score: nilaiRapor, class: namaKelas } ] }.
+const scoreSiswa = (arr: any) => {
+  let newObj: any = { lulus: [], gagal: [] }
+  arr.map((val: any) => {
+    if(val.score > 75) {
+      newObj.lulus.push(val)
+    } else {
+      newObj.gagal.push(val)
+    }
+  })
+  return newObj
+}
+console.log(scoreSiswa([{ name: 'ikhsan', score: 100, class: '1A' }, { name: 'rahma', score: 50, class: '1B' }, { name: 'ridwan', score: 90, class: '1A' }]))
 
 
 
@@ -20,3 +47,13 @@
 // Buatlah sebuah fungsi yang menerima data nilai siswa dalam bentuk array of object. Kembalikanlah array of object yang hanya berisi data siswa yang lulus dimana minimal score yang lulus adalah 75.
 // Contoh output :
 // [ { nama: “Ucup”, skor: 90 }, …dst ]
+const passSiswa = (arr: any) => {
+  let newArr:any = []
+  arr.map((val: any) => {
+    if(val.skor >= 75){
+      newArr.push(val)
+    }
+  })
+  return newArr
+}
+console.log(passSiswa([{nama:'ucup', skor: 90},{nama:'hasan', skor: 70},{nama:'isan', skor: 100}]));
--- typescript/exercise/object-in-array.ts
+++ typescript/exercise/object-in-array.ts
@@ -3,6 +3,21 @@
 // Inputan akan berupa array dengan struktur [ [ nama lengkap (hanya 2 suku kata), gender, tahun lahir ] ].
 // Hitung umur mahasiswa dari tahun lahir yang diinput.
 // Struktur output yang diharapkan yaitu => [ { firstName: nama depan, lastName: namaBelakang, age: umur }, … ]
+const objInArr = (arr: any[]) => {
+  let currentYear = new Date().getFullYear()
+  let newArr: any = []
+  arr.map((val: any, index: number) => {
+      let age = currentYear - val[2]
+      let name = val[0].split(' ')
+      newArr.push({
+          firstName: name[0],
+          lastName: name[1],
+          age
+      })
+  })
+  return newArr
+}
+console.log(objInArr([['Michael Jackson', 'Male', 2000], ['Michael 2', 'Male', 2002]]))
 
 
 
