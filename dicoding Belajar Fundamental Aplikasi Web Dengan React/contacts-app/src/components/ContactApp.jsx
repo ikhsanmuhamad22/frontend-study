967e858e396123d0ec1c5f2c7a0089bd4c8bbb60
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2024-01-07 08:35:56 +0700

    contactapp: memproteksi fitur contact

--- dicoding Belajar Fundamental Aplikasi Web Dengan React/contacts-app/src/components/ContactApp.jsx
+++ dicoding Belajar Fundamental Aplikasi Web Dengan React/contacts-app/src/components/ContactApp.jsx
@@ -4,21 +4,47 @@ import Navigation from './Navigation';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
 
-function ContactApp() {
-  return (
-    <div className="contact-app">
-      <header className="contact-app__header">
-        <h1>Aplikasi Kontak</h1>
-        <Navigation />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/add" element={<AddPage />} />
-        </Routes>
-      </main>
-    </div>
-  );
+class ContactApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authedUser: null,
+    };
+  }
+
+  render() {
+    if (this.state.authedUser === null) {
+      return (
+        <div className="contact-app">
+          <header className="contact-app__header">
+            <h1>Aplikasi Kontak</h1>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/*" element={<p>Halaman Login</p>} />
+              <Route path="/register" element={<p>Halaman Register</p>} />
+            </Routes>
+          </main>
+        </div>
+      );
+    }
+
+    return (
+      <div className="contact-app">
+        <header className="contact-app__header">
+          <h1>Aplikasi Kontak</h1>
+          <Navigation />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/add" element={<AddPage />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default ContactApp;
